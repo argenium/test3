f57cf54d05a7c4f745696a64e28d70729e3ce53d
Sebastien Nobert <sebastien.nobert@guavus.com>
2017-05-23 13:52:06 -0400

    Feature/opsmtl 99 fix (#137)
    * OPSMTL-99. Fully idempotent + first item

--- zabbix_config.py
+++ zabbix_config.py
@@ -46,17 +46,14 @@ EXAMPLES = '''
     state: "absent"
 '''
 
-try :
+import json
+HAS_REQUESTS = False
+try:
     import requests
-    HAS_REQUESTS=True
-except:
-    HAS_REQUESTS=False
-
-try :
-    import json
-    HAS_JSON=True
-except:
-    HAS_JSON=False
+except ImportError:
+    pass
+else:
+    HAS_REQUESTS = True
 
 
 class ZabbixConfigException(Exception):
@@ -90,10 +87,13 @@ class ZabbixConfig(object):
         else:
             self.id_string = "{}id".format(self.api)
 
+        self.session = requests.Session()
+        self.session.headers.update({'Content-Type': 'application/json-rpc',
+                                     'Cache-Control': 'no-cache'})
         try:
-            self.session = requests.Session()
-            self.session.headers.update({'Content-Type': 'application/json-rpc', 'Cache-Control': 'no-cache'})
-            self.auth = self.do_request("user.login", {'user': self.user, 'password': self.password})
+            self.auth = self.do_request("user.login",
+                                        {'user': self.user,
+                                         'password': self.password})
         except Exception as e:
             raise(ZabbixConfigException("Cannot login to zabbix api at {} ({})".format(self.url, e)))
 
@@ -103,7 +103,7 @@ class ZabbixConfig(object):
             raise(ZabbixConfigException("Exception while checking if {} \"{}\" exist [params {}]({})".format(self.api, self.name, self.api_args, e)))
 
     def do_request(self, method, params=None):
-        post_data = {'jsonrpc': '2.0', 'method': method, 'params': params , 'id': self.request_id}
+        post_data = {'jsonrpc': '2.0', 'method': method, 'params': params, 'id': self.request_id}
         if self.auth is not None:
             post_data['auth'] = self.auth
 
@@ -117,7 +117,7 @@ class ZabbixConfig(object):
         return data['result']
 
     def get_object(self, params):
-        params["filter"] = { self.api_uid : self.name }
+        params["filter"] = {self.api_uid : self.name}
         self.check_api_args(params)
         result = self.do_request("{}.get".format(self.api), params)
         if len(result) > 0:
@@ -137,17 +137,23 @@ class ZabbixConfig(object):
             params['selectInterfaces'] = 'extend'
         if 'hosts' in self.api_args:
             params['selectHosts'] = 'hostid'
+        if 'hostid' in self.api_args:
+            params['filter']['hostid'] = self.api_args['hostid']
 
     def sort_api_args(self):
         if 'templates' in self.api_args:
             self.object['templates'] = self.object['parentTemplates']
             self.object['templates'] = sorted(self.object['templates'], key=lambda k: k['templateid'])
+            self.api_args['templates'] = sorted(self.api_args['templates'], key=lambda k: k['templateid'])
         if 'groups' in self.api_args:
             self.object['groups'] = sorted(self.object['groups'], key=lambda k: k['groupid'])
+            self.api_args['groups'] = sorted(self.api_args['groups'], key=lambda k: k['groupid'])
         if 'interfaces' in self.api_args:
-            self.object['interfaces'] = sorted(self.object['interfaces'], key=lambda k: k['interfaceid'])
+            self.object['interfaces'] = sorted(self.object['interfaces'], key=lambda k: k['ip'])
+            self.api_args['interfaces'] = sorted(self.api_args['interfaces'], key=lambda k: k['ip'])
         if 'hosts' in self.api_args:
             self.object['hosts'] = sorted(self.object['hosts'], key=lambda k: k['hostid'])
+            self.api_args['hosts'] = sorted(self.api_args['hosts'], key=lambda k: k['hostid'])
 
     def update_object(self):
         has_changed = False
@@ -157,7 +163,7 @@ class ZabbixConfig(object):
             self.api_args[self.id_string] = self.id
             result = self.do_request("{}.update".format(self.api), self.api_args)
             has_changed = True
-        meta = { "name": self.name, self.id_string: self.id }
+        meta = {"name": self.name, self.id_string: self.id}
         return (has_changed, meta)
 
     def list_to_dict(self, l):
@@ -169,12 +175,13 @@ class ZabbixConfig(object):
             if k not in orig:
                 need_update = True
                 continue
-            if type(update[k]) != list and type(update[k]) != dict and update[k] != orig[k]:
+            if type(update[k]) != list and \
+                    type(update[k]) != dict and update[k] != orig[k]:
                 need_update = True
                 continue
             if type(update[k]) != type(orig[k]):
-                if (type(update[k]) != str and type(update[k]) != unicode) or \
-                    (type(orig[k]) != str and type(orig[k]) != unicode) :
+                if (type(update[k]) != str and type(update[k]) != unicode) \
+                        or (type(orig[k]) != str and type(orig[k]) != unicode):
                     need_update = True
                     continue
             if type(orig[k]) == dict:
@@ -200,7 +207,7 @@ class ZabbixConfig(object):
                 result = self.do_request("{}.create".format(self.api), self.api_args)
                 has_changed = True
                 self.id = result["{}s".format(self.id_string)][0]
-                meta = { "name": self.name, self.id_string: self.id }
+                meta = {"name": self.name, self.id_string: self.id}
             else:
                 has_changed, meta = self.update_object()
         except Exception as e:
@@ -215,11 +222,11 @@ class ZabbixConfig(object):
         if self.id is None:
             return (False, {})
 
-        try :
+        try:
             result = self.do_request("{}.delete".format(self.api), [self.id])
             if len(result) > 0:
                 has_changed = True
-                meta = { "name": self.name, self.id_string: self.id }
+                meta = {"name": self.name, self.id_string: self.id}
         except Exception as e:
             raise(ZabbixConfigException("Exception while deleting {} {} ({})".format(self.api, self.name, e)))
 
@@ -230,27 +237,22 @@ def main():
     has_changed = False
     result = {}
 
-    fields = {
-        "zabbix_url": {"required": True, "type": "str"},
-        "zabbix_user": {"required": True, "type": "str"},
-        "zabbix_password": {"required": True, "type": "str"},
-        "api": {"required": True, "type": "str" },
-        "api_uid": {"required": True, "type": "str" },
-        "api_args": {"required": True, "type": "dict"},
-        "state": {
-            "default": "present",
-            "choices": ["present", "absent"],
-            "type": "str"
-        },
-    }
-
+    module = AnsibleModule(
+        argument_spec = dict(
+            zabbix_url = dict(required=True, type="str"),
+            zabbix_user = dict(required=True, type="str"),
+            zabbix_password = dict(required=True, type="str", no_log=True),
+            api = dict(required=True, type="str"),
+            api_uid = dict(required=True, type="str"),
+            api_args = dict(required=True, type="dict"),
+            state = dict(default="present",
+                       choices=["present", "absent"], type="str")
+        ),
+        supports_check_mode=True
+    )
     if (HAS_REQUESTS is False):
-        module.fail_json(msg="Missing required requests module (check docs or install with: pip install requests)")
-    if (HAS_JSON is False):
-        module.fail_json(msg="Missing required json module (check docs or install with: pip install requests)")
-
-    module = AnsibleModule(argument_spec=fields, supports_check_mode=True)
-
+        module.fail_json(msg="'requests' package not found... \
+                         you can try install using pip: pip install requests")
     try:
         config = ZabbixConfig(module)
 
@@ -262,6 +264,7 @@ def main():
         module.fail_json(msg="{}".format(e))
 
     module.exit_json(changed=has_changed, meta=result)
+
 
 from ansible.module_utils.basic import AnsibleModule
 if __name__ == '__main__':
