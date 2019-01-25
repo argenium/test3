2bbab3e4fe4c48dc38fcb54f6ae51068afa81097
Ernest Zheryakov <ernest.zheryakov@guavus.com>
2019-01-25 10:43:02 -0500

    edited

--- roles/zabbix/zabbix-config/library/zabbix_config.py
+++ roles/zabbix/zabbix-config/library/zabbix_config.py
@@ -2,6 +2,7 @@
 
 # Copyright (c) 2017 [Guavus]
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
